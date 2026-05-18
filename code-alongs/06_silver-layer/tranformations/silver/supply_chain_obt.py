673dc5510bcf203e3485e9e7fc67a8f4118d7412
LisaYllander92 <Lisaylander92@gmail.com>
2026-05-18 11:57:17 +0000

    finish tasks from code along and update pipeline

--- code-alongs/06_silver-layer/tranformations/silver/supply_chain_obt.py
+++ code-alongs/06_silver-layer/tranformations/silver/supply_chain_obt.py
@@ -1,31 +1,64 @@
 from pyspark import pipelines as dp
-from pyspark.sql.functions import col, coalesce, when, lit
+from pyspark.sql.functions import coalesce, lit, when, col, to_timestamp, round
 from utils.utils import rename_column_to_snake_case
 
-@dp.table(name="supply_chain_demo.silver.supply_chain_obt", 
-          comment="Cleaned supply chain data for DataCo",
-          table_properties={
-              "delta.columnMapping.mode": "name",
-              "delta.minReaderVersion": "2",
-              "delta.minWriterVersion": "5"
-          })
 
-def cleaned_supply_chain_data():
-    df = rename_column_to_snake_case(spark.readStream.table("supply_chain_demo.bronze.raw_supply_chain"))
-    
+@dp.table(
+    name="supply_chain_demo.silver.supply_chain_obt",
+    comment="Cleaned supply chain data for DataCo",
+    table_properties={
+        "delta.columnMapping.mode": "name",
+        "delta.minReaderVersion": "2",
+        "delta.minWriterVersion": "5",
+    },
+)
+def cleaned_supply_chain():
+    df = rename_column_to_snake_case(
+        spark.sql("SELECT * FROM STREAM supply_chain_demo.bronze.raw_supply_chain")
+    )
+
     return (
-    df.withColumn("customer_lname", coalesce(col("customer_lname"), lit("-")))
-    .withColumn(
-        "customer_zipcode",
-        coalesce(col("customer_zipcode").cast("string"), lit("unknown")),
+        df.withColumn("customer_lname", coalesce("customer_lname", lit("-")))
+        .withColumn(
+            "shipping_date",
+            to_timestamp(col("shipping_date_(dateorders)"), "M/d/yyyy H:mm"),
+        )
+        .withColumn(
+            "customer_zipcode",
+            coalesce(col("customer_zipcode").cast("string"), lit("unknown")),
+        )
+        .withColumn(
+            "order_zipcode",
+            coalesce(col("order_zipcode").cast("string"), lit("unknown")),
+        )
+        .withColumn(
+            "customer_country",
+            when(col("customer_country") == "EE. UU.", "United States").otherwise(
+                col("customer_country")
+            ),
+        )
+        .withColumn(
+            "order_item_product_price", round(col("order_item_product_price"), 2)
+        )
+        .withColumn("benefit_per_order", round(col("benefit_per_order"), 2))
+        .withColumn("Sales_per_customer", round(col("sales_per_customer"), 2))
+        .withColumn("product_price", round(col("product_price"), 2))
+        .withColumn("order_item_discount", round(col("order_item_discount"), 2))
+        .withColumn(
+            "order_item_discount_rate", round(col("order_item_discount_rate"), 2)
+        )
+        .withColumn(
+            "order_item_profit_ratio", round(col("order_item_profit_ratio"), 2)
+        )
+        .withColumn(
+            "order_profit_per_order", round(col("order_profit_per_order"), 2)
+        )
+        .withColumn("order_item_total", round(col("order_item_total"), 2))
+        .withColumn("sales", round(col("sales"), 2))
+        .drop(
+            "product_description",
+            "customer_email",
+            "customer_password",
+            "shipping_date_(dateorders)",
+        )
     )
-    .withColumn(
-        "order_zipcode", coalesce(col("order_zipcode").cast("string"), lit("unknown"))
-    )
-    .withColumn(
-        "customer_country",
-        when(col("customer_country") == "EE. UU.", "United States").otherwise(
-            col("customer_country")
-        ),
-    )
-).drop("product_description", "customer_email", "customer_password", "shipping_date_(dateorders)")
